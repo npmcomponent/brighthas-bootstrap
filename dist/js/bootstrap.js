bbc40e6565386ec53742921375b24c3f6159c8dc
Toby Ho <airportyh@gmail.com>
2014-02-20 10:48:39 -0500

    Automatically rewritten for npm.

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -26,7 +26,7 @@
  * limitations under the License.
  * ======================================================================== */
 
-var jQuery =  require("jquery");
+var jQuery =  require('component-jquery');
 
 +function ($) { "use strict";
 
